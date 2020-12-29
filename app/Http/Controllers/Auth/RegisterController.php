3bf2c80295848432f4714fc87fa2156fad234bfc
codingvesna <codingvesna1@gmail.com>
2020-12-29 22:25:46 +0100

    store user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,22 +14,20 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request) {
-        // dd('abc');
-        // dd($request);
-        // dd($request->get('email'));
-        // dd($request->email);
-
-        // validation
             $this->validate($request, [
                 'name' => 'required|max:255',
                 'username' => 'required|max:255',
                 'email' => 'required|email|max:255',
                 'password' => 'required|confirmed'
             ]);
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
 
-            dd('store');
-        // store user
-        // sign the user in
-        // redirect
+        return redirect()->route('dashboard');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use \App\Http\Controllers\Auth\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']); // name is inherited
 
