eba43cf9ad8eec21b158c6cefaf07ed44ac2e76f
codingvesna <codingvesna1@gmail.com>
2020-12-29 22:13:31 +0100

    output old session data - register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
                 <div class="mb-4">
                     <label for="name" class="sr-only">Name</label>
                     <input type="text" name="name" id="name" placeholder="Your name" class="bg-gray-100
-                    border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+                    border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{ old('name') }}">
 
                     @error('name')
                         <div class="text-red-500 mt-2 text-sm">
@@ -20,8 +20,8 @@
 
                 <div class="mb-4">
                     <label for="username" class="sr-only">Username</label>
-                    <input type="text" name="username" id="username" placeholder="Your name" class="bg-gray-100
-                    border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+                    <input type="text" name="username" id="username" placeholder="Username" class="bg-gray-100
+                    border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{ old('username') }}">
 
                     @error('username')
                     <div class="text-red-500 mt-2 text-sm">
@@ -33,7 +33,7 @@
                 <div class="mb-4">
                     <label for="email" class="sr-only">Email Address</label>
                     <input type="text" name="email" id="email" placeholder="Your email" class="bg-gray-100
-                    border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+                    border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="{{ old('email') }}">
 
                     @error('email')
                     <div class="text-red-500 mt-2 text-sm">
@@ -57,7 +57,8 @@
                 <div class="mb-4">
                     <label for="password_confirmation" class="sr-only">Password again</label>
                     <input type="text" name="password_confirmation" id="password_confirmation" placeholder="Repeat your password"
-                     class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror" value="">
+                     class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('name') border-red-500 @enderror"
+                           value="">
 
                     @error('password_confirmation')
                     <div class="text-red-500 mt-2 text-sm">
