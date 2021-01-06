785089f5f046ce798328c934f0b96612117c2da7
codingvesna <codingvesna1@gmail.com>
2021-01-06 17:21:43 +0100

    PostController, route hooked

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Auth\RegisterController;
 /*
@@ -34,6 +35,7 @@ Route::post('/login', [LoginController::class, 'store']);
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']); // name is inherited
 
-Route::get('/posts', function () {
-    return view('posts.index');
-});
+
+
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');;
