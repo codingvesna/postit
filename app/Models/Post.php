917fc6f5fcd3042c5edb6f22da705906890f9639
codingvesna <codingvesna1@gmail.com>
2021-01-10 16:22:47 +0100

    Like Model, Post/Like Relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,7 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function likes(){
+        return $this->hasMany(Like::class);
+    }
 }
